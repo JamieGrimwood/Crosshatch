50861ecfc8f58bbd6c83e1c5786b68e034373a64
Jamie_ <volcanomonster07@gmail.com>
2022-02-13 23:59:42 +0000

    Removing terminal div

--- controller.js
+++ controller.js
@@ -6,7 +6,6 @@ module.exports = {
         return new Promise((resolve, reject) => {
             docker.listContainers({ all: true }, function (err, containers) {
                 if (err) return reject(err)
-
                 resolve(containers)
             });
         });
