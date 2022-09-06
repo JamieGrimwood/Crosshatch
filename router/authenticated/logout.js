6f09108e84e4de8e705109352e554cce53aac109
JamieGrimwood <jgrimwood09@gmail.com>
2022-09-06 15:26:39 +0200

    starting new structure

--- router/authenticated/logout.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/logout', async (req, res) => {
-    res.session.destroy()
-    res.redirect('/')
-})
-
-module.exports = router;
